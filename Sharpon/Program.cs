5ffa58a9595d6b21fad356186cd879f168831ee5
Aaron_LW <aaronl.waechter@gmail.com>
2025-12-28 12:34:00 +0100

    Im remaking this shit

--- Sharpon/Program.cs
+++ Sharpon/Program.cs
@@ -1,13 +1,2 @@
-using System.IO;
-
-public static class Program
-{
-    static void Main(string[] args)
-    {
-        string fileToOpen = args.Length > 0 ? Path.GetFullPath(args[0]) : null;
-    
-        using var game = new Sharpon.Game1(fileToOpen);    
-        game.Run();
-    
-    }
-}
+﻿using var game = new Sharpon.Game1();
+game.Run();
