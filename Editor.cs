d3f694962434eb4c23e9411b7d499ad44edcc6ee
AaronoLW <aaronl.waechter@gmail.com>
2026-09-18 14:33:25 +0200

    More editing

--- Editor.cs
+++ Editor.cs
@@ -8,7 +8,7 @@ using Color = System.Drawing.Color;
 public class Editor(string initialText = "")
 {
     public string Text { get; private set; } = initialText;
-    public int CharIndex = 0;
+    public int CharIndex = initialText.Length;
 
     private static readonly Vector2 Offset = new(20);
 
@@ -18,19 +18,34 @@ public class Editor(string initialText = "")
         if (Input.TextInput != null)
         {
             stringBuilder.Append(Input.TextInput);
+            CharIndex += Input.TextInput.Length;
         }
 
         if (Input.IsKeyPressed(SDL.Keycode.Backspace))
         {
-            if (stringBuilder.Length > 0)
+            if (CharIndex > 0)
             {
-                stringBuilder.Remove(stringBuilder.Length - 1, 1);
+                stringBuilder.Remove(CharIndex - 1, 1);
+                CharIndex--;
             }
         }
 
         if (Input.IsKeyPressed(SDL.Keycode.Return))
         {
             stringBuilder.Append('\n');
+            CharIndex++;
+        }
+
+        if (Input.IsKeyPressed(SDL.Keycode.Left))
+        {
+            if (CharIndex > 0)
+                CharIndex--;
+        }
+
+        if (Input.IsKeyPressed(SDL.Keycode.Right))
+        {
+            if (CharIndex < Text.Length)
+                CharIndex++;
         }
 
         Text = stringBuilder.ToString();
@@ -46,11 +61,62 @@ public class Editor(string initialText = "")
 
     private Vector2 GetCaretPosition()
     {
-        Vector2 textSize = App.Font.MeasureString(Text, App.POINT_SIZE);
+        Vector2 textSize = new(0, App.Font.MeasureString(Text[0..CharIndex], App.POINT_SIZE).Y);
 
-        if (Text.Length > 0)
+        if (CharIndex > 0 && Text[CharIndex - 1] != '\n')
+        {
             textSize.Y -= App.Font.MeasureString("W", App.POINT_SIZE).Y;
+        }
+
+        string selectedLine = GetSelectedLineUntilCaret();
+        textSize.X = App.Font.MeasureString(selectedLine, App.POINT_SIZE).X;
+
+        if (selectedLine == "\n")
+        {
+            textSize.X -= App.Font.MeasureString("\n", App.POINT_SIZE).X;
+        }
 
         return Offset + textSize;
+    }
+
+    //private int GetLineIndex()
+    //{
+    //    if (Text.Length == 0) return 0;
+
+    //    int newLineCount = 0;
+    //    for (int i = 0; i < CharIndex; i++)
+    //    {
+    //        if (Text[i] == '\n')
+    //            newLineCount++;
+    //    }
+
+    //    return newLineCount;
+    //}
+
+    private string GetSelectedLineUntilCaret()
+    {
+        if (Text.Length == 0) return "";
+
+        int startIndex = 0;
+        for (int i = CharIndex - 1; i > 0; i--)
+        {
+            if (Text[i] == '\n')
+            {
+                startIndex = i;
+                break;
+            }
+        }
+
+        int endIndex = CharIndex;
+        //for (int i = CharIndex; i < Text.Length; i++)
+        //{
+        //    if (Text[i] == '\n')
+        //    {
+        //        endIndex = i;
+        //        break;
+        //    }
+        //}
+
+        return Text[startIndex..endIndex];
     }
 }
