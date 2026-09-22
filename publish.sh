a2b5918cda193ad2a5a98656b8eb27d8a6a04358
AaronoLW <aaronl.waechter@gmail.com>
2026-09-23 00:56:46 +0200

    Fix publish script

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 mkdir -p build
 
 dotnet publish
-cp -r bin/Release/net10.0/linux-x64/native/* build
-rm build/Sharpon.dbg
+cp -r bin/Release/net10.0/linux-x64/native/Sharpon build
+cp -r Assets build
