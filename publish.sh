c294e1ebd95311113cdae2b395df81bf7a3cc60a
AaronoLW <aaronl.waechter@gmail.com>
2026-09-23 23:35:08 +0200

    Separate windows and linux builds
    Because windows builds definitely exist trust

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
-mkdir -p build
+mkdir -p build/Linux
 
 dotnet publish
-cp bin/Release/net10.0/linux-x64/native/Sharpon build
-cp -r Assets build
+cp bin/Release/net10.0/linux-x64/native/Sharpon build/Linux
+cp -r Assets build/Linux
