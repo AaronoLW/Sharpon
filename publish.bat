8eafe915175ce7498a90bb04c3a8f895b93bd6f4
AaronoLW <aaronl.waechter@gmail.com>
2026-09-23 23:54:13 +0200

    Fuck microslop!

--- publish.bat
+++ publish.bat
@@ -6,4 +6,4 @@ dotnet publish
 copy bin\Release\net10.0\win-x64\publish\Sharpon.exe build\Windows
 copy bin\Release\net10.0\win-x64\publish\SDL3* build\Windows
 copy Assets build\Windows\Assets
-@REM fuck microslop
+@REM fuck microslop!
